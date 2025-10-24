63ab711709131997abc498ee445a9873ac4540ba
MiglaniDhruv <dgmiglani@gmail.com>
2025-10-25 02:28:28 +0530

    deploying

--- server/index.ts
+++ server/index.ts
@@ -157,6 +157,12 @@ app.use(cors({
   ],
   credentials: true,
 }));
+
+app.get("/", (req, res) => {
+  res.redirect("https://fruit-stand-frontend.vercel.app/pune-fresh/login");
+});
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
