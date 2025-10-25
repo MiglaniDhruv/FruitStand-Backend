7142475d528a43f8eb8894c2ff873da6c7811ced
MiglaniDhruv <dgmiglani@gmail.com>
2025-10-25 13:05:42 +0530

    deploying

--- server/index.ts
+++ server/index.ts
@@ -210,12 +210,8 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   next();
 });
 
-// Use "type: module" in package.json to use ES modules
-import express from 'express';
-const app = express();
+
 const port = 3000;
- 
-// Define your routes
 app.get('/', (req, res) => {
   res.json({ message: 'Hello from Express on Vercel!' });
 });
